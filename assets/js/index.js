7a1f2bb382537582265cdc27f7510004be92a29e
RicardoG79 <jr.gonseg@gmail.com>
2025-04-02 20:03:32 -0300

    cambio de nombre

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelector('.nav-links');
 
 burger.addEventListener('click', () => {
-  nav.classList.toggle('nav-active');
+  navLinks.classList.toggle('nav-active');
   burger.classList.toggle('toggle');
 });
 
